6586d029a46ce35643beea207a6f9c90daed05f0
EDrTech <erik@rudectech.com>
2024-08-24 08:44:52 +0200

    Power saving features added
    Wake up from power down mode every 5 seconds to take measurements.
    Wake up on button interrupt.
    
    This realizes big power saving gains as the CPU is powered down most of the time.

--- pmg001_default/pmg001_default.ino
+++ pmg001_default/pmg001_default.ino
@@ -1,252 +1,54 @@
-#include <Wire.h>
-#include <avr/interrupt.h>
 #include <avr/sleep.h>
-
-
-
-#define ADS1015_ADDRESS 0x48
-#define TMP102_ADDRESS 0x49
-#define INA219_ADDRESS 0x40
-unsigned long secondCountdown1 = 0, secondCountdown2 = 0;
-bool pa7held = false;
-bool sw = false;
-bool actionTaken = false;
-bool released = true;
-bool LEDstate = LOW;
-
+#include <avr/interrupt.h>
 #include <Wire.h>
 
 #define INA219_ADDRESS 0x40
-#define TMP102_ADDRESS 0x48
+#define TMP102_ADDRESS 0x49
 #define ADS1015_ADDRESS 0x48
 
-// Function to set calibration for INA219
-void INA219_setCal() {
-  uint16_t calByte = 4096;                    // Calibration register value
-  uint16_t configBytes = 0b0011100110011111;  // Configuration register value
+bool pa7_pressed = 0, pa3_pressed = 0, io_wake = 0, out_active = 0;
+int pa7_pressedfor = 0, pa3_pressedfor = 0, pa7_pressedon = 0, pa3_pressedon = 0, pa7_presstime = 0, pa3_presstime = 0, rtc_ctdn = 0;
 
-  // Write to calibration register
-  Wire.beginTransmission(INA219_ADDRESS);
-  Wire.write(0x05);                       // Calibration register address
-  Wire.write((uint8_t)(calByte >> 8));    // High byte
-  Wire.write((uint8_t)(calByte & 0xFF));  // Low byte
-  Wire.endTransmission();
+void setup() 
+{
+    Serial.begin(115200);
+    RTC_init();    
+    PORTA.PIN3CTRL = PORT_PULLUPEN_bm | PORT_ISC_BOTHEDGES_gc;  // Enable pull-up and interrupt on both edges for PA3
+    PORTA.PIN7CTRL = PORT_PULLUPEN_bm | PORT_ISC_BOTHEDGES_gc;  // Enable pull-up and interrupt on both edges for PA7
+    pinConfigure(PIN_PA6,(PIN_DIR_OUTPUT | PIN_PULLUP_ON | PIN_OUT_HIGH));
+    pinConfigure(PIN_PA2,(PIN_DIR_OUTPUT | PIN_PULLUP_OFF | PIN_OUT_LOW));  
+    PORTA.INTFLAGS = PORT_INT3_bm | PORT_INT7_bm;  // Clear any existing interrupt flags for PA3 and PA7
 
-  // Write to configuration register
-  Wire.beginTransmission(INA219_ADDRESS);
-  Wire.write(0x00);                           // Configuration register address
-  Wire.write((uint8_t)(configBytes >> 8));    // High byte
-  Wire.write((uint8_t)(configBytes & 0xFF));  // Low byte
-  Wire.endTransmission();
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);  // Set sleep mode to Power Down mode
+    Wire.begin();
+    sei();  // Enable global interrupts
 }
 
-// Function to read bus voltage from INA219
-float INA219_readBV() {
-  float resV = 0;
-  uint16_t busVoltReg = 0x00;
-  INA219_setCal();
-
-  // Request bus voltage register
-  Wire.beginTransmission(INA219_ADDRESS);
-  Wire.write(0x02);  // Bus voltage register address
-  Wire.endTransmission();
-
-  // Read bus voltage
-  Wire.requestFrom(INA219_ADDRESS, 2);
-  if (Wire.available() == 2) {
-    busVoltReg = (Wire.read() << 8) | Wire.read();
-  }
-
-  resV = ((busVoltReg >> 3) * 0.001) * 4;
-  return resV;
-}
-
-// Function to read shunt voltage from INA219
-float INA219_readSV() {
-  float resV = 0;
-  uint16_t shuntVoltReg = 0x00;
-  INA219_setCal();
-
-  // Request shunt voltage register
-  Wire.beginTransmission(INA219_ADDRESS);
-  Wire.write(0x01);  // Shunt voltage register address
-  Wire.endTransmission();
-
-  // Read shunt voltage
-  Wire.requestFrom(INA219_ADDRESS, 2);
-  if (Wire.available() == 2) {
-    shuntVoltReg = (Wire.read() << 8) | Wire.read();
-  }
-
-  resV = shuntVoltReg * 0.1;
-  return resV;
-}
-
-// Function to read current from INA219
-float INA219_readC() {
-  float resC = 0;
-  uint16_t currentReg = 0x00;
-  INA219_setCal();
-
-  // Request current register
-  Wire.beginTransmission(INA219_ADDRESS);
-  Wire.write(0x04);  // Current register address
-  Wire.endTransmission();
-
-  // Read current
-  Wire.requestFrom(INA219_ADDRESS, 2);
-  if (Wire.available() == 2) {
-    currentReg = (Wire.read() << 8) | Wire.read();
-  }
-
-  resC = currentReg;
-  return resC;
-}
-
-// Function to read temperature from TMP102
-float readTMP102() {
-  int16_t dTemp;
-  float reading = 0;
-
-  // Request temperature register
-  Wire.beginTransmission(TMP102_ADDRESS);
-  Wire.write(0X00);  // Temperature register address
-  Wire.endTransmission();
-
-  uint8_t regByte[2];
-
-  // Read temperature
-  Wire.requestFrom(TMP102_ADDRESS, 2);
-  regByte[0] = Wire.read();
-  regByte[1] = Wire.read();
-
-  dTemp = ((regByte[0]) << 4) | (regByte[1] >> 4);
-  if (dTemp > 0x7FF) {
-    dTemp |= 0xF000;
-  }
-  reading = dTemp * 0.0625;
-  return reading;
-}
-
-// Function to read a specific channel from ADS1015
-float ADS1015_readChannel(uint8_t channel) {
-  uint16_t mux;
-  switch (channel) {
-    case 0:
-      mux = 0x4000;
-      break;
-    case 1:
-      mux = 0x5000;
-      break;
-    case 2:
-      mux = 0x6000;
-      break;
-    case 3:
-      mux = 0x7000;
-      break;
-    default:
-      return -1;  // Invalid channel
-  }
-
-  float resV = 0;
-  uint16_t voltReg = 0x00;
-  uint16_t configBytes = 0x0000;
-
-  // Set up configuration bytes for ADS1015
-  configBytes |= 0x0200;  // Set gain and mode
-  configBytes |= mux;     // Set channel
-  configBytes |= 0x0080;  // Set data rate
-  configBytes |= 0x8000;  // Start single conversion
-
-  // Write to configuration register
-  Wire.beginTransmission(ADS1015_ADDRESS);
-  Wire.write(0x01);                           // Configuration register address
-  Wire.write((uint8_t)(configBytes >> 8));    // High byte
-  Wire.write((uint8_t)(configBytes & 0xFF));  // Low byte
-  Wire.endTransmission();
-
-  delay(10);  // Wait for conversion to complete
-
-  // Request conversion result
-  Wire.beginTransmission(ADS1015_ADDRESS);
-  Wire.write(0x00);  // Conversion register address
-  Wire.endTransmission();
-
-  // Read conversion result
-  Wire.requestFrom(ADS1015_ADDRESS, 2);
-  if (Wire.available() == 2) {
-    voltReg = (Wire.read() << 8) | Wire.read();
-  }
-
-  resV = (voltReg >> 4);
-  return resV * (4.096f / (32768 >> 4));
-}
-
-// Setup function
-void setup() {
-  Serial.begin(115200);
-
-  pinConfigure(PIN_PA7, (PIN_DIR_INPUT | PIN_PULLUP_ON));                  // Configure PWR_SW
-  pinConfigure(PIN_PA3, (PIN_DIR_INPUT | PIN_PULLUP_ON));                  // Configure SWITCH
-  pinConfigure(PIN_PA6, (PIN_DIR_OUTPUT | PIN_PULLUP_ON | PIN_OUT_HIGH));  // Configure P_ON
-  pinConfigure(PIN_PA2, (PIN_DIR_OUTPUT | PIN_PULLUP_OFF | PIN_OUT_LOW));  // Configure LED
-
-  Wire.begin();
-  delay(100);  // Allow time for setup
-}
-
-void loop() {
-  if (digitalRead(PIN_PA7)) {
-    // Button is released
-    secondCountdown2 = millis();
-    pa7held = false;
-    released = true;
-    actionTaken = false;  // Reset the action taken flag
-  } else {
-    // Button is pressed
-    if (!pa7held) {
-      // First time button is pressed
-      secondCountdown2 = millis();
-      pa7held = true;
-      released = false;
-    } else if (!actionTaken) {
-      // Button is held and action has not been taken yet
-      if (sw == 0 && (millis() - secondCountdown2) >= 500) {
-        // Held for more than 500ms
-        secondCountdown2 = millis();
-        digitalWrite(PIN_PA6, LOW);
-        sw = 1;
-        actionTaken = true;
-      } else if (sw == 1 && (millis() - secondCountdown2) >= 3000) {
-        // Held for more than 3000ms
-        secondCountdown2 = millis();
-        digitalWrite(PIN_PA6, HIGH);
-        sw = 0;
-        actionTaken = true;
-      }
+void RTC_init() 
+{
+    while (RTC.STATUS > 0)
+    {
+        ;  // Wait for all registers to be synchronized
     }
-  }
-  static bool lastButtonStatePA3 = HIGH;
-  bool readingPA3 = digitalRead(PIN_PA3);
-  if (readingPA3 == LOW && lastButtonStatePA3 == HIGH) {
-    // PA3 was just pressed, toggle PA2
-    LEDstate = !LEDstate;
-    digitalWrite(PIN_PA2, LEDstate);
-  }
-  lastButtonStatePA3 = readingPA3;
-  if (millis() - secondCountdown1 >= 1000) {
-    float tbuf, ain0buf, ain1buf, ain2buf, ain3buf, bvbuf, svbuf, bcbuf;
-    // Read sensor values
-    tbuf = readTMP102();
-    ain0buf = ADS1015_readChannel(0);
-    ain1buf = ADS1015_readChannel(1);
-    ain2buf = ADS1015_readChannel(2);
-    ain3buf = ADS1015_readChannel(3);
-    bvbuf = INA219_readBV();
-    svbuf = INA219_readSV();
-    bcbuf = INA219_readC();
-    Serial.write(12);
-    String output = "";  // Clear screen and move cursor to home
+    RTC.CLKSEL = RTC_CLKSEL_INT32K_gc;  // 32.768kHz Internal Ultra-Low-Power Oscillator (OSCULP32K)
+    RTC.PITINTCTRL = RTC_PI_bm;  // PIT Interrupt: enabled
+    RTC.PITCTRLA = RTC_PERIOD_CYC32768_gc | RTC_PITEN_bm;  // Enable PIT counter: enabled
+}
+
+ISR(RTC_PIT_vect) 
+{
+    RTC.PITINTFLAGS = RTC_PI_bm;  // Clear interrupt flag by writing '1' (required)
+    float tbuf = readTMP102();
+    float ain0buf = ADS1015_readChannel(0);
+    float ain1buf = ADS1015_readChannel(1);
+    float ain2buf = ADS1015_readChannel(2);
+    float ain3buf = ADS1015_readChannel(3);
+    float bvbuf = INA219_readBV();
+    float svbuf = INA219_readSV();
+    float bcbuf = INA219_readC();
+
+    Serial.write(12);  // Clear screen and move cursor to home
+     String output = "";  // Clear screen and move cursor to home
     output += "-----------------------------------\r\n";
     output += "TMP102 Temperature:      ";
     output += String(tbuf, 2);
@@ -276,6 +78,213 @@ void loop() {
     output += "mA\r\n";
     output += "-----------------------------------\r\n";
     Serial.print(output);
-    secondCountdown1 = millis();
-  }
+    rtc_ctdn = millis();
+}
+
+void INA219_setCal() 
+{
+    uint16_t calByte = 4096;  // Calibration register value
+    uint16_t configBytes = 0b0011100110011111;  // Configuration register value
+
+    Wire.beginTransmission(INA219_ADDRESS);
+    Wire.write(0x05);  // Calibration register address
+    Wire.write((uint8_t)(calByte >> 8));  // High byte
+    Wire.write((uint8_t)(calByte & 0xFF));  // Low byte
+    Wire.endTransmission();
+
+    Wire.beginTransmission(INA219_ADDRESS);
+    Wire.write(0x00);  // Configuration register address
+    Wire.write((uint8_t)(configBytes >> 8));  // High byte
+    Wire.write((uint8_t)(configBytes & 0xFF));  // Low byte
+    Wire.endTransmission();
+}
+
+float INA219_readBV() 
+{
+    uint16_t busVoltReg = 0x00;
+    INA219_setCal();
+
+    Wire.beginTransmission(INA219_ADDRESS);
+    Wire.write(0x02);  // Bus voltage register address
+    Wire.endTransmission();
+
+    Wire.requestFrom(INA219_ADDRESS, 2);
+    if (Wire.available() == 2) 
+    {
+        busVoltReg = (Wire.read() << 8) | Wire.read();
+    }
+
+    return ((busVoltReg >> 3) * 0.001) * 4;
+}
+
+float INA219_readSV() 
+{
+    uint16_t shuntVoltReg = 0x00;
+    INA219_setCal();
+
+    Wire.beginTransmission(INA219_ADDRESS);
+    Wire.write(0x01);  // Shunt voltage register address
+    Wire.endTransmission();
+
+    Wire.requestFrom(INA219_ADDRESS, 2);
+    if (Wire.available() == 2) 
+    {
+        shuntVoltReg = (Wire.read() << 8) | Wire.read();
+    }
+
+    return shuntVoltReg * 0.1;
+}
+
+float INA219_readC() 
+{
+    uint16_t currentReg = 0x00;
+    INA219_setCal();
+
+    Wire.beginTransmission(INA219_ADDRESS);
+    Wire.write(0x04);  // Current register address
+    Wire.endTransmission();
+
+    Wire.requestFrom(INA219_ADDRESS, 2);
+    if (Wire.available() == 2) 
+    {
+        currentReg = (Wire.read() << 8) | Wire.read();
+    }
+
+    return currentReg;
+}
+
+float readTMP102() 
+{
+    int16_t dTemp;
+    Wire.beginTransmission(TMP102_ADDRESS);
+    Wire.write(0X00);  // Temperature register address
+    Wire.endTransmission();
+
+    uint8_t regByte[2];
+    Wire.requestFrom(TMP102_ADDRESS, 2);
+    regByte[0] = Wire.read();
+    regByte[1] = Wire.read();
+
+    dTemp = ((regByte[0]) << 4) | (regByte[1] >> 4);
+    if (dTemp > 0x7FF) 
+    {
+        dTemp |= 0xF000;
+    }
+    return dTemp * 0.0625;
+}
+
+float ADS1015_readChannel(uint8_t channel) 
+{
+    uint16_t mux;
+    switch (channel) 
+    {
+        case 0: mux = 0x4000; break;
+        case 1: mux = 0x5000; break;
+        case 2: mux = 0x6000; break;
+        case 3: mux = 0x7000; break;
+        default: return -1;
+    }
+
+    uint16_t configBytes = 0x0000 | 0x0200 | mux | 0x0080 | 0x8000;
+    uint16_t voltReg = 0x00;
+
+    Wire.beginTransmission(ADS1015_ADDRESS);
+    Wire.write(0x01);  // Configuration register address
+    Wire.write((uint8_t)(configBytes >> 8));  // High byte
+    Wire.write((uint8_t)(configBytes & 0xFF));  // Low byte
+    Wire.endTransmission();
+
+    delay(10);  // Wait for conversion to complete
+
+    Wire.beginTransmission(ADS1015_ADDRESS);
+    Wire.write(0x00);  // Conversion register address
+    Wire.endTransmission();
+
+    Wire.requestFrom(ADS1015_ADDRESS, 2);
+    if (Wire.available() == 2) 
+    {
+        voltReg = (Wire.read() << 8) | Wire.read();
+    }
+
+    return (voltReg >> 4) * (4.096f / (32768 >> 4));
+}
+
+void loop() 
+{
+    sleep_mode();  // Put the MCU to sleep
+    Serial.begin(115200);
+
+    if(io_wake) 
+    {
+        while(!digitalRead(PIN_PA7)) 
+        {
+            pa7_presstime = millis() - pa7_pressedon;
+            if((pa7_presstime >= 500) && (out_active == 0)) 
+            {
+                Serial.println("500ms Timer ON hit");
+                out_active = 1;
+                break;
+            }
+            if((pa7_presstime >= 3000) && (out_active == 1)) 
+            {
+                Serial.println("3s Timer OFF hit");
+                out_active = 0;
+                break;
+            }
+        }
+
+        while(!digitalRead(PIN_PA3)) 
+        {
+            pa3_presstime = millis() - pa3_pressedon;
+            if(pa3_presstime >= 100) 
+            {
+                digitalWrite(PIN_PA2, !digitalRead(PIN_PA2));
+                Serial.println("100ms LED_BUILTIN toggle hit");
+                break;
+            }
+        }
+    }
+
+    io_wake = 0;
+    digitalWrite(PIN_PA6, out_active ? 0 : 1);
+    Serial.flush();
+}
+
+ISR(PORTA_PORT_vect) 
+{
+    io_wake = 1;
+    Serial.begin(115200);
+
+    if (PORTA.INTFLAGS & PORT_INT3_bm) 
+    {
+        delay(10);
+        if(!digitalRead(PIN_PA3)) 
+        {
+            pa3_pressed = 1;
+            pa3_pressedon = millis();
+        } 
+        else 
+        {
+            pa3_pressed = 0;
+            pa3_presstime = 0;
+        }
+    }
+
+    if (PORTA.INTFLAGS & PORT_INT7_bm) 
+    {
+        delay(10);
+        if(!digitalRead(PIN_PA7)) 
+        {
+            pa7_pressed = 1;
+            pa7_pressedon = millis();
+        } 
+        else 
+        {
+            pa7_pressed = 0;
+            pa7_presstime = 0;
+        }
+    }
+
+    Serial.flush();
+    PORTA.INTFLAGS = PORT_INT3_bm | PORT_INT7_bm;
 }
